6544b1755994451c6b85f4c969dc90de531dbf31
Jayesh Sharma <wjayesh@outlook.com>
2021-11-05 18:31:19 +0530

    add comments

--- demo/server/server.js
+++ demo/server/server.js
@@ -1,11 +1,17 @@
+// import express
 const express = require('express')
 const app = express()
+
+// you can choose any port number of your choice.
+// confirm if the port is available
 const port = 3000
 
+// Define a function for the base url
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// start listening on the port for requests
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
